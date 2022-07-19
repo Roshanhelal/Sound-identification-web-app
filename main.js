72faa73151a5544e1a3e9e2b6dd3ca4920b4cdfd
Roshanhelal <98272607+Roshanhelal@users.noreply.github.com>
2022-07-19 20:54:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //https://teachablemachine.withgoogle.com/models/2P7yo-Y2E/
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    })
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/2P7yo-Y2E/model.json',modelready)
+
+}
+
+function modelready(){
+    classifier.classify(gotResults)
+}
